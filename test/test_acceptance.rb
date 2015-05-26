e028df9b07cbbf30fe223f19e3f458f4d111f179
Josh Cheek <josh.cheek@gmail.com>
2015-05-26 17:47:42 -0600

    Executable works

--- test/test_acceptance.rb
+++ test/test_acceptance.rb
@@ -1,18 +1,42 @@
 require 'tmpdir'
+require 'open3'
 
 class TestAcceptance < Minitest::Test
+  ProcessResult = Struct.new :stdout, :stderr, :status do
+    def exitstatus
+      status.exitstatus
+    end
+  end
+
+  def invoke(executable, *args, from:)
+    bin_path      = File.expand_path '../bin', __dir__
+    original_path = ENV['PATH']
+    Dir.chdir from do
+      ENV['PATH'] = "#{bin_path}:#{original_path}"
+      ProcessResult.new(*Open3.capture3(executable, *args))
+    end
+  ensure
+    ENV['PATH'] = original_path
+  end
+
   def test_acceptance
-    order = []
+    Dir.mktmpdir do |dir|
+      File.write "#{dir}/Minirakefile", <<-RAKEFILE
+        task default: :e
+        task(:a)              { puts :a }
+        task(b: :a)           { puts :b }
+        task(c: :b)           { puts :c }
+        task(d: :c)           { puts :d }
+        task(e: [:a, :b, :d]) { puts :e }
+        task(:e) { puts "ENV['ABC']: \#{ENV['ABC'].inspect}" }
+        task(:e) { puts File.basename(__FILE__), __LINE__ }
+      RAKEFILE
 
-    minirake = Minirake.new
-    minirake.task default: :e
-    minirake.task(:a)              { order << :a }
-    minirake.task(b: :a)           { order << :b }
-    minirake.task(c: :b)           { order << :c }
-    minirake.task(d: :c)           { order << :d }
-    minirake.task(e: [:a, :b, :d]) { order << :e }
-    minirake.call []
-
-    assert_equal [:a, :b, :c, :d, :e], order
+      result = invoke 'minirake', 'ABC=DEF', from: dir
+      assert_equal "", result.stderr
+      assert_equal "a\nb\nc\nd\ne\nENV['ABC']: \"DEF\"\nMinirakefile\n8\n",
+                   result.stdout
+      assert_equal 0, result.exitstatus
+    end
   end
 end
