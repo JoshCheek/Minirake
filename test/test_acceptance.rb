e62d8e4d7d97f9bfe6cbb3a619b135e84f7a3d9e
Josh Cheek <josh.cheek@gmail.com>
2015-05-26 18:00:07 -0600

    Show we can define and call methods without fucking up global env, refactor acceptance test

--- test/test_acceptance.rb
+++ test/test_acceptance.rb
@@ -22,20 +22,47 @@ class TestAcceptance < Minitest::Test
   def test_acceptance
     Dir.mktmpdir do |dir|
       File.write "#{dir}/Minirakefile", <<-RAKEFILE
-        task default: :e
-        task(:a)              { puts :a }
-        task(b: :a)           { puts :b }
-        task(c: :b)           { puts :c }
-        task(d: :c)           { puts :d }
-        task(e: [:a, :b, :d]) { puts :e }
-        task(:e) { puts "ENV['ABC']: \#{ENV['ABC'].inspect}" }
-        task(:e) { puts File.basename(__FILE__), __LINE__ }
+        task :correct_file_and_line do
+          puts File.basename(__FILE__), __LINE__
+        end
+
+        task(:a)                     { puts :a }
+        task(b: :a)                  { puts :b }
+        task(c: :b)                  { puts :c }
+        task(d: :c)                  { puts :d }
+        task(ordering: [:a, :b, :d]) { puts :ordering }
+
+        task :env_vars_get_set do
+          puts "ENV['ABC']: \#{ENV['ABC'].inspect}"
+        end
+
+        def the_method; "Method got defined"; end
+        task :can_define_methods do
+          puts the_method
+          puts "My class: \#{self.class}"
+          puts "Method is on Minirake: \#{Minirake.instance_methods.include?(:the_method)}"
+          puts "Method is on singleton class: \#{singleton_methods.include? :the_method}"
+        end
+
+        task default: :correct_file_and_line
+        task default: :ordering
+        task default: :can_define_methods
+        task default: :env_vars_get_set
       RAKEFILE
 
       result = invoke 'minirake', 'ABC=DEF', from: dir
+
       assert_equal "", result.stderr
-      assert_equal "a\nb\nc\nd\ne\nENV['ABC']: \"DEF\"\nMinirakefile\n8\n",
+
+      assert_equal "Minirakefile\n2\n"                     +
+                   "a\nb\nc\nd\nordering\n"                +
+                   "Method got defined\n"                  +
+                   "My class: Minirake\n"                  +
+                   "Method is on Minirake: false\n"        +
+                   "Method is on singleton class: true\n"  +
+                   "ENV['ABC']: \"DEF\"\n",
                    result.stdout
+
       assert_equal 0, result.exitstatus
     end
   end
